b5bfcd2a24fc9ab129e7e7d6e550346f7714c4d0
Tetsu45 <htetmyetkyaw163@gmail.com>
2025-07-02 14:59:11 +0900

    Won/lost message modified

--- rolling_two_dices.py
+++ rolling_two_dices.py
@@ -16,11 +16,11 @@ def main():
     print("Rolling dice...")
     print(f"Die 1: {die1}")
     print(f"Die 2: {die2}")
-    print(f"Total: {die1 + die2}")
+    print(f"Total value: {die1 + die2}")
     if CheckIfGreaterThan7(die1, die2):
-        print("You won!")
+        print(f"{name} won!")
     else:
-        print("You lost!")
+        print(f"{name} lost!")
     
 if __name__ == "__main__":
     main()
